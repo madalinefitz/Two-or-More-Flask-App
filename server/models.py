fdec80be8ff7e590181155ddb6343dfadd0fce0c
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-06 12:56:12 -0400

    Story model cleanup

--- server/models.py
+++ server/models.py
@@ -1,9 +1,7 @@
-from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
-from config import db, bcrypt
+from config import db
 from sqlalchemy.orm import validates
-from datetime import datetime
+
 
 class Story(db.Model, SerializerMixin):
     __tablename__ = 'stories'
@@ -13,4 +11,3 @@ class Story(db.Model, SerializerMixin):
     last_name_initial = db.Column(db.String, nullable=False)
     story = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    
